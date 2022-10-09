fcb84db605240bf0e07f111008b140e862757f56
Reejo Joseph <92742868+ReejoJoseph1244@users.noreply.github.com>
2022-10-09 19:55:35 +0530

    Created a layout for register activity

--- app/src/main/java/com/example/drawernavigation/RegisterActivity.kt
+++ app/src/main/java/com/example/drawernavigation/RegisterActivity.kt
@@ -2,10 +2,29 @@ package com.example.drawernavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var btnSubmit : Button
+    lateinit var email : EditText
+    lateinit var regex : Regex
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btnSubmit = findViewById(R.id.btnSubmit)
+        email = findViewById(R.id.inputemail)
+        btnSubmit.setOnClickListener {
+            regex = Regex("""\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}""")
+            if(android.util.Patterns.EMAIL_ADDRESS.matcher(email.text).matches()){
+                Toast.makeText(this,"Registered Successfully",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Enter valid email",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
